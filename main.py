61b56493a007deec7e1354d45e150ed50b1b5303
KeilaHadama <hadamakeila@gmail.com>
2026-06-15 19:37:52 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# importar biblioteca do pygame
+
+import pygame
+
+print('Setup Start')
+pygame.init()
+#criar janela
+window = pygame.display.set_mode(size=(600, 480))
+print('Setup End')
+
+print('Loop Start')
+while True:
+    #Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() #Close Window
+            quit() #end pygame
+            
